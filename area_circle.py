d0831a137b3fb99b7c0a33d98d6d43412eaa6588
William Hatfield <whatfield@haverford.edu>
2024-02-01 11:01:34 -0500

    Calculates the area of a circle

---
+++ area_circle.py
@@ -0,0 +1,5 @@
+#Calculate the area of a circle with a given radius
+import numpy as np
+r = float(input("What is the radius of the circle?"))
+area = np.pi*r**2
+print("The area of the circle is ", area)
